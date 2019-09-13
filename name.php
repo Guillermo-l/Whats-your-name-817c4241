f10310f858efe9b3f379e685efc516ddb673b402
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-13 14:25:06 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Wat is je voornaam? \n";
+
+$voornaam = readline();
+
+echo "Wat is je achternaam? \n";
+
+$achternaam = readline();
+
+print("Jouw naam is: " . $voornaam . " " . $achternaam)
+
+?>
